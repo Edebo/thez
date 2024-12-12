871ac0609ee051197936d54f5cbf9efcb08e7b7c
edebo <akandeadebowale0@gmail.com>
2024-12-12 22:21:17 +0100

    home insurance

--- app.js
+++ app.js
@@ -7,12 +7,48 @@ import websiteScraper from 'website-scraper';
 const url = 'https://www.thezebra.com/'; // Replace with the URL of the website you want to scrape
 
 //
-//let path =["auto-insurance",]
+let path =["insurance/car/prefill/start/","insurance/car/prefill/start/details/"]
+
+
+let all =[{
+  url:"https://www.thezebra.com/",
+  dir:"./home"
+},
+{
+  url:"https://www.thezebra.com/insurance/car/prefill/start/",
+  dir:"./start"
+},
+{
+  url:"https://www.thezebra.com/insurance/car/prefill/start/details/",
+  dir:"./details"},
+  {
+    url:"https://www.thezebra.com/insurance/car/vehicles/select",
+    dir:"./details"},{
+      url:"https://www.thezebra.com/insurance/car/prefill/start/details/",
+      dir:"./details"},
+      {
+        url:"https://www.thezebra.com/insurance/car/prefill/start/details/",
+        dir:"./details"},
+        {
+          url:"https://www.thezebra.com/insurance/car/prefill/start/details/",
+          dir:"./details"}
+
+]
+"/car/prefill/vehicles/select/": false,
+                                "/car/prefill/vehicles/details/": false,
+                                "/car/prefill/drivers/details/": false,
+                                "/car/prefill/coverage-selection/"
+
+
+
+
+for (let i=0;i<all.length;i++){
+// Start the scraping process
 
 // Configure options for website-scraper
 const options = {
-  urls: [url],
-  directory: './home', // The directory where the downloaded content will be saved
+  urls: [all[i].url],
+  directory:`./${all[i].dir}`, // The directory where the downloaded content will be saved
   subdirectories: [
     { directory: 'img', extensions: ['.jpg', '.png', '.svg', '.gif'] },
     { directory: 'js', extensions: ['.js'] },
@@ -21,7 +57,7 @@ const options = {
   ],
 };
 
-// Start the scraping process
 websiteScraper(options)
   .then(() => console.log('Website successfully scraped and downloaded!'))
   .catch((err) => console.error('An error occurred:', err));
+}
